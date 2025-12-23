1593ee22d104ffdd6099c37300c9e07957705a1e
MenachemKazido <agbg7172@gmail.com>
2025-12-23 16:12:46 +0200

    End of oop models

--- models/CRUD.py
+++ models/CRUD.py
@@ -3,17 +3,18 @@ from connection import Connection
 class Crud:
 
     @staticmethod
-    def create_contact(first_name: str, last_name: str, phone_number: str):
+    def create_contact(data):
         coon = Connection().get_connection()
         cursor = coon.cursor()
         cursor.execute("""
         INSERT INTO contacts 
         (first_name, last_name, phone_number) 
-        VALUES (%s,%s,%s) """,(first_name,last_name,phone_number))
+        VALUES (%s,%s,%s) """,(data["first_name"],data["last_name"],data["phone_number"]))
         coon.commit()
         cursor.close()
         coon.close()
         return {"message": "Contact created successfully"}
+
 
     @staticmethod
     def get_all_contacts():
@@ -26,8 +27,44 @@ class Crud:
         return result
 
 
+    @staticmethod
+    def update_contact(item_id: int,data):
+        coon = Connection().get_connection()
+        cursor = coon.cursor()
+        cursor.execute(f"""
+                UPDATE contacts
+                SET first_name = %s ,last_name = %s ,phone_number = %s
+                WHERE id  = %s """,
+                (data["first_name"],data["last_name"],data["phone_number"],item_id))
+        coon.commit()
+        cursor.close()
+        cursor.close()
+        return {"message":"true"}
 
-print(Crud.get_all_contacts())
+
+    @staticmethod
+    def delete_contact(item_id: int):
+        coon = Connection().get_connection()
+        cursor = coon.cursor()
+        cursor.execute("""DELETE FROM contacts
+                            WHERE id = %s""",(item_id,))
+        coon.commit()
+        cursor.close()
+        coon.close()
+        return {"message":"true"}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- models/contact.py
+++ models/contact.py
@@ -1,14 +1,17 @@
 
 class Contact:
-    def __init__(self,first_name: str,last_name: str,phone_number: str):
+    def __init__(self,first_name: str,last_name: str,phone_number: str,contact_id: int=None,):
+        self.contact_id = contact_id
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
 
-    def get_contact(self):
+    def contact_to_dict(self):
         return {
+            "item_id":self.contact_id,
             "first_name":self.first_name,
             "last_name":self.last_name,
             "phone_number":self.phone_number
         }
+
 
